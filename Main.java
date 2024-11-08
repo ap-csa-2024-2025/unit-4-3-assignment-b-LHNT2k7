b60dddf4662470eb15d64d862a432fd56b55e480
LHNT2k7 <ltri9573@gmail.com>
2024-11-08 10:00:39 -0800

    sdfghj

--- Main.java
+++ Main.java
@@ -4,14 +4,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    
   }
 
   public static boolean isPrime(int N)
   {
-    // TODO: Replace and write your code below
-    return false;
+    if (N == 1)
+    {
+      return false;
+    }
+    for (int i =2; i< N; i++)
+    {
+      if (n % i ==0)
+      {
+        return false;
+      }
+    }
   }
+  return true;
 }
